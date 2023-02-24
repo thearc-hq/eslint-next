248de8e267e5d046d590ddc4782d5789443b52b9
Nico Hülscher <25116822+eweren@users.noreply.github.com>
2023-02-24 15:46:02 +0100

    chore: remove yaml

--- index.js
+++ index.js
@@ -5,9 +5,42 @@
 'use strict';
 
 module.exports = {
-  extends: ['./default.yml'],
   rules: {
     'react-hooks/exhaustive-deps': 'off',
     'react/react-in-jsx-scope': 'off',
+    "react-in-jsx-scope": 'off'
+  },
+  reportUnusedDisableDirectives: true,
+  ignorePatterns: ['**/node_modules/', '**/dist/', '**/build/', '**/*.config.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'jsdoc',
+    'react',
+    'sort-keys-fix'
+  ],
+  extends: [
+    '@the-arc-gmbh/eslint-config',
+    'plugin:import/recommended',
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    "plugin:prettier/recommended",
+    'next/core-web-vitals',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
   },
 };
